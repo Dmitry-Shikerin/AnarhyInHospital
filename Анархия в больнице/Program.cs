aab84d88e0ad3013b7a8fbad410dc74979364af5
Dmitry-Shikerin <klubnr4ok@mail.ru>
2023-09-01 12:03:28 +0300

    Добавьте файлы проекта.

---
+++ Анархия в больнице/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Анархия_в_больнице
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hospital hospital = new Hospital();
+
+            hospital.Work();
+        }
+    }
+
+    class Patient
+    {
+        public Patient(string name, int age, string disease)
+        {
+            Name = name;
+            Age = age;
+            Disease = disease;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Disease { get; private set; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Имя: {Name}, Возраст: {Age}, Заболевание: {Disease}");
+        }
+    }
+
+    class Hospital
+    {
+        private List<Patient> _patients;
+
+        public Hospital()
+        {
+            _patients = Create();
+        }
+
+        public void Work()
+        {
+            const string CommandSortByName = "1";
+            const string CommandSortByAge = "2";
+            const string CommandSortByDisease = "3";
+
+            bool isWork = true;
+
+            while (isWork)
+            {
+                ShowInfo();
+
+                Console.WriteLine();
+                Console.WriteLine($"{CommandSortByName} - Сортиравка по имени");
+                Console.WriteLine($"{CommandSortByAge} - Сортировать по возрасту");
+                Console.WriteLine($"{CommandSortByDisease} - Сортировать по болезни");
+                string userInput = Console.ReadLine();
+
+                switch (userInput)
+                {
+                    case CommandSortByName:
+                        SortByName();
+                        break;
+
+                    case CommandSortByAge:
+                        SortByAge();
+                        break;
+
+                    case CommandSortByDisease:
+                        SortByDisease();
+                        break;
+
+                    default:
+                        Console.WriteLine("Некорректный ввод");
+                        break;
+                }
+
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+
+        private void SortByName()
+        {
+            Console.WriteLine("Введите имя");
+            string introducedName = Console.ReadLine();
+
+            var sortedList = _patients.Where(patient => patient.Name == introducedName);
+
+            foreach (Patient patient in sortedList)
+            {
+                patient.ShowInfo();
+            }
+        }
+
+        private void SortByAge()
+        {
+            Console.WriteLine("Введите возраст");
+            int introducedAge = ReadAge();
+
+            var sortedList = _patients.Where(patient => patient.Age == introducedAge);
+
+            foreach (Patient patient in sortedList)
+            {
+                patient.ShowInfo();
+            }
+        }
+
+        private void SortByDisease()
+        {
+            Console.WriteLine("Введите заболевание");
+            string introducedDisease = Console.ReadLine();
+
+            var sortedList = _patients.Where(patient => patient.Disease == introducedDisease);
+
+            foreach (Patient patient in sortedList)
+            {
+                patient.ShowInfo();
+            }
+        }
+
+        private List<Patient> Create()
+        {
+            List<Patient> patients = new List<Patient>()
+            {
+                new Patient("Елизавета", 19, "Камни в почках"),
+                new Patient("Валерий", 25, "Апендицит"),
+                new Patient("Аркадий", 35, "Язва"),
+                new Patient("Мария", 65, "Тошнота"),
+                new Patient("Елизавета", 47, "Апендицит"),
+                new Patient("Эдуард", 63, "Язва"),
+                new Patient("Светлана", 49, "Апендицит"),
+                new Patient("Валерий", 34, "Тошнота"),
+                new Patient("Елизавета", 28, "Камни в почках"),
+                new Patient("Мария", 48, "Апендицит"),
+            };
+
+            return patients;
+        }
+
+        private void ShowInfo()
+        {
+            Console.WriteLine("Список всех больных\n");
+
+            foreach (Patient patient in _patients)
+            {
+                patient.ShowInfo();
+            }
+        }
+
+        private int ReadAge()
+        {
+            int number = 0;
+
+            bool result = false;
+
+            while (result == false)
+            {
+                result = int.TryParse(Console.ReadLine(), out number);
+
+                if (result == false)
+                {
+                    Console.WriteLine("Ошибка. Введите год.");
+                }
+            }
+
+            return number;
+        }
+    }
+}
---
+++ Анархия в больнице/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Анархия в больнице")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Анархия в больнице")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("893c7f14-d420-4fb0-8cea-749abdf8f954")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
